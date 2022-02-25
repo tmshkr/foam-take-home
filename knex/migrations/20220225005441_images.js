6c9ebfe6606902ab51fde5ea5c7504b445d87476
Tim Shaker <tim@tmshkr.com>
2022-02-24 18:19:08 -0800

    migrations

---
+++ knex/migrations/20220225005441_images.js
@@ -0,0 +1,10 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("images", (table) => {
+    table.string("key").primary();
+    table.boolean("is_foaming");
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.raw("DROP TABLE images CASCADE");
+};
